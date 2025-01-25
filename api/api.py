fdd305fb8a739f97e6d5269b7f377af00fc8cd69
Yann <227698+yanncarlier@users.noreply.github.com>
2025-01-25 12:47:02 +0700

    modified:   api/api.py and added api/Bitcoin_Address_Generation_API.yaml

--- api/api.py
+++ api/api.py
@@ -1,13 +1,16 @@
 from typing import Union
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
+from fastapi import FastAPI, HTTPException, Depends, Body
+from typing import Optional
 # from pydantic import BaseModel
 app = FastAPI()
-####################################################
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
-####################################################
+############################
+class MnemonicRequest(BaseModel):
+    numWords: Optional[int] = 24
+class MnemonicResponse(BaseModel):
+    BIP39Mnemonic: str
+    BIP39Seed: str
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -19,8 +22,9 @@ def read_root():
 # def update_item(item_id: int, item: Item):
 #     return {"item_name": item.name, "item_id": item_id}
 ####################################################
-@app.get("/mnemonic/")
-def function_name():
+# Endpoint for generating mnemonic
+@app.get("/generate-mnemonic", response_model=MnemonicResponse)
+async def generate_mnemonic():
     from mnemonic import Mnemonic
     mnemonic = Mnemonic("english")
     #Generate word list given the strength (128 - 256):
@@ -28,7 +32,6 @@ def function_name():
     # Given the word list generate seed:
     seed = mnemonic.to_seed(words)
     return {
-        "BIP39 Mnemonic": mnemonic.generate(128),
-        "BIP39 Seed": seed.hex()
+        "BIP39Mnemonic": mnemonic.generate(128),
+        "BIP39Seed": seed.hex()
         }
-################################
