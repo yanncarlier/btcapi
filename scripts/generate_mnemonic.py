55f4258588af442a70ea466c86e613a07cda4791
Yann <227698+yanncarlier@users.noreply.github.com>
2025-04-09 10:09:06 +0700

    output the BIP32 Root Key in scripts/generate_mnemonic.py

--- scripts/generate_mnemonic.py
+++ scripts/generate_mnemonic.py
@@ -1,14 +1,18 @@
-#https://github.com/trezor/python-mnemonic
+# https://github.com/trezor/python-mnemonic
 from mnemonic import Mnemonic
+import bip32utils  # Ensure you have this library installed
+
 mnemonic = Mnemonic("english")
-# print('++++++++++++++++++++++++++++++++++++++++++++++')
-# print(mnemonic.generate(128))
 print('++++++++++++++++++++++++++++++++++++++++++++++')
-#Generate word list given the strength (128 - 256):
+# Generate word list given the strength (128 - 256):
 words = mnemonic.generate(strength=128)
 print('BIP39 Mnemonic: %s' % words)
 print('++++++++++++++++++++++++++++++++++++++++++++++')
-#Given the word list generate seed:
+# Given the word list generate seed:
 seed = mnemonic.to_seed(words)
 print('BIP39 Seed: %s' % seed.hex())
+print('++++++++++++++++++++++++++++++++++++++++++++++')
+# Generate BIP32 Root Key:
+bip32_root_key = bip32utils.BIP32Key.fromEntropy(seed)
+print('BIP32 Root Key: %s' % bip32_root_key.ExtendedKey())
 print('++++++++++++++++++++++++++++++++++++++++++++++')
