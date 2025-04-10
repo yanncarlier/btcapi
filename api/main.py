ee15afb478c3a5afe26110317ac5b57f9d2c6b59
Yann <227698+yanncarlier@users.noreply.github.com>
2025-04-10 11:04:56 +0700

    change origin from localhost to 127.0.0.1

--- api/main.py
+++ api/main.py
@@ -43,7 +43,7 @@ app = FastAPI(
 
 # Replace with your frontend's origin
 origins = [
-    "http://localhost:3000",
+    "http://127.0.0.1:8000",
     "https://7bitcoin-txcom.vercel.app",
     "https://btc-tx-gw.bitcoin-tx.com"
 ]
